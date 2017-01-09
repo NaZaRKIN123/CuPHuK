1cd6aa7e081e86da95a73756bbb29db24dec9873
NaZaRKIN123 <nazarkin124@gmail.com>
2017-01-09 20:40:14 +0200

    remove not used 'usings'

--- CuPHuK/Program.cs
+++ CuPHuK/Program.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
 
 
 namespace CuPHuK
@@ -15,6 +10,7 @@ namespace CuPHuK
             UserInput inputLine = new UserInput(Console.ReadLine());
             inputLine.SplitToArguments();
             inputLine.Print();
+            inputLine.CalculateAllQueues();
         }
     }
 }
