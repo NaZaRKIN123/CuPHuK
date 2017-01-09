03b0b1a6ea3ab515c8d1b1f78ea944ddae4ceb96
NaZaRKIN123 <nazarkin124@gmail.com>
2017-01-09 15:20:50 +0200

    add Queue class

---
+++ CuPHuK/Queue.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuPHuK
+{
+    class Queue
+    {
+        public decimal CalculateArguments(ArrayList argumentsHolder)
+        {
+            decimal result = 0;
+            return result;
+        }
+    }
+}
