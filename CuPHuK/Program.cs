f76f5d829a94fde2118aa1c74640a5ee2e6d2ccd
NaZaRKIN123 <nazarkin124@gmail.com>
2017-03-10 21:09:32 +0200

    Update Program

--- CuPHuK/Program.cs
+++ CuPHuK/Program.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 
 namespace CuPHuK
 {
@@ -7,10 +7,35 @@ namespace CuPHuK
     {
         static void Main(string[] args)
         {
-            UserInput inputLine = new UserInput(Console.ReadLine());
-            inputLine.SplitToArguments();
-            inputLine.Print();
-            inputLine.CalculateAllQueues();
+            string input = "2*(3+4)+(5*6)/3";
+
+            //parse the input
+            List<string> arguments = new List<string>(UserInput.Parse(input));
+            UserInput.Print(arguments);
+
+            //do Math
+            bool hasBrackets = true;
+            string result = "";
+            while (hasBrackets)
+            {
+                if (arguments.Contains("("))
+                {
+                    hasBrackets = true;
+                    //get one line and strip the brackets
+                    List<string> queue = BracketsManager.GetQueue(arguments);
+                    //save start position where to put result back later
+                    int startOfBracketsPosition = BracketsManager.BracketsPosition(arguments)[0];
+                    BracketsManager.RemoveQueue(ref arguments);
+                    result = MathManager.Calculate(queue);
+                    arguments.Insert(startOfBracketsPosition, result);
+                }
+                else
+                {
+                    hasBrackets = false;
+                    result = MathManager.Calculate(arguments);
+                } 
+            }
+            Console.WriteLine("\nThe result is: {0}\n", result);
         }
     }
 }
